fe2cde6cfcdb9da92a901b8095dff0d02eb7104a
Yannick Pereira-Reis <yannick@zol.fr>
2016-08-18 12:12:00 +0200

    Fixing startup script for multiple restart

--- nginx/1.0/scripts/start.sh
+++ nginx/1.0/scripts/start.sh
@@ -2,8 +2,7 @@
 
 if [ "$IS_SILEX" == "true" ]; then
   echo " SILEX PROJECT AND NGINX CONF"
-  rm -rf /etc/nginx/sites-available/default.conf
-  mv /etc/nginx/sites-available/silex.conf /etc/nginx/sites-available/default.conf
+  cat /etc/nginx/sites-available/silex.conf > /etc/nginx/sites-available/default.conf
 else
   echo "SYMFONY PROJECT AND NGINX CONF"
   rm -rf /etc/nginx/sites-available/silex.conf
