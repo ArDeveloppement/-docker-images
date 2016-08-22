ec5c5f4bf58f03617040b525c17ca09748d63476
Yannick Pereira-Reis <yannick@zol.fr>
2016-08-22 14:48:16 +0200

    Fixing nginx startup script

--- nginx/1.0/scripts/start.sh
+++ nginx/1.0/scripts/start.sh
@@ -4,8 +4,7 @@ if [ "$IS_SILEX" == "true" ]; then
   echo " SILEX PROJECT AND NGINX CONF"
   cat /etc/nginx/sites-available/silex.conf > /etc/nginx/sites-available/default.conf
 else
-  echo "SYMFONY PROJECT AND NGINX CONF"
-  rm -rf /etc/nginx/sites-available/silex.conf
+  echo "SYMFONY PROJECT AND NGINX CONF (Nothing to do because default file is for SF)"
 fi
 
 exec $*
