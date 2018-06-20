2e0f3ef775fd08621e0257c23017343d8f8dca9c
Florian Rey <florian.rey@elao.com>
2018-06-20 13:26:01 +0200

    Add html site config

--- nginx/1.2/scripts/start.sh
+++ nginx/1.2/scripts/start.sh
@@ -3,6 +3,9 @@
 if [ "$IS_SILEX" == "true" ]; then
   echo " SILEX PROJECT AND NGINX CONF"
   cat /etc/nginx/sites-available/silex.conf > /etc/nginx/sites-available/default.conf
+elif [ "$IS_HTML" == "true" ]; then
+  echo " HTML PROJECT AND NGINX CONF"
+  cat /etc/nginx/sites-available/html.conf > /etc/nginx/sites-available/default.conf
 else
   echo "SYMFONY PROJECT AND NGINX CONF (Nothing to do because default file is for SF)"
 fi
