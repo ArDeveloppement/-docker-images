bca6c34b1a26677b887750f2826b57db509ce3f7
Florian Rey <florian.rey@elao.com>
2018-06-20 13:13:43 +0200

    Copy from 1.1.1

---
+++ nginx/1.2/scripts/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ "$IS_SILEX" == "true" ]; then
+  echo " SILEX PROJECT AND NGINX CONF"
+  cat /etc/nginx/sites-available/silex.conf > /etc/nginx/sites-available/default.conf
+else
+  echo "SYMFONY PROJECT AND NGINX CONF (Nothing to do because default file is for SF)"
+fi
+
+exec $*
