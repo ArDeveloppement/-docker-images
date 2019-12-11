ed45bc5a33f1cf82244f724da9de9768213810b6
root <root@LAPNXT016.interne.nextsys.fr>
2019-12-11 14:19:54 +0100

    add afs_sync image

--- afs_sync/1.0/scripts/start.sh
+++ afs_sync/1.0/scripts/start.sh
@@ -1,18 +1,18 @@
-#!/bin/ash
+#!/bin/sh
 
 set -e
 
 # User / Group
-if [[ -n ${USER_ID} || -n ${GROUP_ID} ]]; then
+if [[ ! -z ${USER_ID} || ! -z ${GROUP_ID} ]]; then
     GROUP_ID=${GROUP_ID:-1000}
-    if [[ ${GROUP_ID} -ne 1000 ]]; then
+    if [[ ${GROUP_ID} != 1000 ]]; then
         printf "[`date +'%F %T'`] system.INFO: Set group id \"${GROUP_ID}\"\n"
     fi
     USER_ID=${USER_ID:-1000}
-    if [[ ${USER_ID} -ne 1000 ]]; then
+    if [[ ${USER_ID} != 1000 ]]; then
         printf "[`date +'%F %T'`] system.INFO: Set user id \"${USER_ID}\"\n"
     fi
-    if [[ ${USER_ID} -ne 1000 || ${GROUP_ID} -ne 1000 ]]; then
+    if [[ ${USER_ID} != 1000 || ${GROUP_ID} != 1000 ]]; then
         deluser azure_sync
         addgroup -g ${GROUP_ID} azure_sync
         adduser -h /config -s /bin/ash -D azure_sync -u ${USER_ID} -G azure_sync
@@ -20,6 +20,4 @@ if [[ -n ${USER_ID} || -n ${GROUP_ID} ]]; then
     fi
 fi
 
-#exec su-exec azure_sync /usr/bin/rsync "$@"
-exec su-exec azure_sync "$@"
-
+exec su-exec azure_sync /usr/bin/rsync "$@"
