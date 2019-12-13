e7e0cc7ccdbbf6a970fe45d4d04beddee2c59379
Spoon4 <spoon4@gmail.com>
2019-12-13 16:22:44 +0100

    afs: update entrypoint

--- afs-sync/1.0/scripts/start.sh
+++ afs-sync/1.0/scripts/start.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ -n "${USER_ID}" || -n "${GROUP_ID}" ]]; then
     GROUP_ID=${GROUP_ID:-1000}
-    if [[ ${GROUP_ID} != 1000 ]]; then
+    if [[ ${GROUP_ID} -ne 1000 ]]; then
         # Check group already exists
         GROUP=$(getent group ${GROUP_ID} | cut -d: -f1)
         if [[ ! ${GROUP} ]]; then
@@ -12,15 +12,16 @@ if [[ -n "${USER_ID}" || -n "${GROUP_ID}" ]]; then
             awk -i inplace -v group_id="${GROUP_ID}" -F ':' -e 'BEGIN { OFS=":" } /^azure_sync/ { if ($3 != group_id) $3=group_id;} { print $0 }' /etc/group
             awk -i inplace -v group_id="${GROUP_ID}" -F ':' -e 'BEGIN { OFS=":" } /^azure_sync/ { if ($4 != group_id) $4=group_id;} { print $0 }' /etc/passwd
          else
-            chgrp -R ${GROUP} /home/azure_sync
+            chgrp -R ${GROUP} /config
+            chgrp -R ${GROUP} /data
         fi   
     fi
     USER_ID=${USER_ID:-1000}
-    if [[ ${USER_ID} != 1000 ]]; then
+    if [[ ${USER_ID} -ne 1000 ]]; then
         printf "[`date +'%F %T'`] system.INFO: Set user id \"${USER_ID}\"\n"
         awk -i inplace -v user_id="${USER_ID}" -F ':' -e 'BEGIN { OFS=":" } /^azure_sync/ { if ($3 != user_id) $3=user_id;} { print $0 }' /etc/passwd
     fi
-    if [[ ${USER_ID} != 1000 || ${GROUP_ID} -ne 1000 ]]; then
+    if [[ ${USER_ID} -ne 1000 || ${GROUP_ID} -ne 1000 ]]; then
         chown -R ${USER_ID}:${GROUP_ID} /config
         chown -R ${USER_ID}:${GROUP_ID} /data
     fi
